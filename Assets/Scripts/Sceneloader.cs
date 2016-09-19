09163ee8ca2e672c5e2f1b842a5db0223af7cfcc
Alexander Scheurer <mail@aspepex.net>
2016-09-19 18:53:37 +0200

    Sceneloading and switching done

--- Assets/Scripts/Sceneloader.cs
+++ Assets/Scripts/Sceneloader.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Sceneloader : MonoBehaviour {
 
+    public Text sceneText;
+
+    private string[] scenes;
+    private int currIndex;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        scenes = new string[4];
+
+        scenes[0] = "1";
+        scenes[1] = "2";
+        scenes[2] = "3";
+        scenes[3] = "4";
+
+        SceneManager.LoadScene("1");
+    }
+
+    public void NextScene()
+    {
+        if (currIndex + 1 >= scenes.Length)
+        {
+            currIndex = 0;
+        }
+        else
+        {
+            currIndex += 1;
+        }
+
+        SceneManager.LoadScene(scenes[currIndex]);
+        sceneText.text = scenes[currIndex];
+    }
+
+    public void PrevScene()
+    {
+        if (currIndex - 1 < 0)
+        {
+            currIndex = scenes.Length - 1;
+        }
+        else
+        {
+            currIndex -= 1;
+        }
+
+        SceneManager.LoadScene(scenes[currIndex]);
+        sceneText.text = scenes[currIndex];
+    }
 }
